bbd81967cc53eb91d51e0715038b885190913119
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-07-12 02:57:39 -0500

    Liquid glass v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import logo from './assets/logo.png'
-import LiquidGlass from 'liquid-glass-react'
+import './components/LiquidGlass.css'
+import DistortionEffect from './components/DistortionEffect.jsx'
 
 function App() {
 
   return (
     <div className='whole-page'>
-      <LiquidGlass className='sidenav' cornerRadius={10} padding={40}>
-        <div className='logo'>
-          {/* <h1>📊</h1> */}
-          <img src={logo} alt='SpaceX Logo' />
+      <DistortionEffect />
+      <div className='sidenav wrapper'>
+        <div className="effect"></div>
+        <div className="tint"></div>
+        <div className="shine"></div>
+        <div className="text">
+          <div className='logo'>
+            <img src={logo} alt='SpaceX Logo' />
+          </div>
+          <h2><span className="material-symbols-outlined">home</span>&nbsp;&nbsp;Dashboard</h2>
+          <h2><span className="material-symbols-outlined">search</span>&nbsp;&nbsp;Search</h2>
+          <h2><span className="material-symbols-outlined">info</span>&nbsp;&nbsp;About</h2>
         </div>
-        <h2><span className="material-symbols-outlined">home</span>&nbsp;&nbsp;Dashboard</h2>
-        <h2><span className="material-symbols-outlined">search</span>&nbsp;&nbsp;Search</h2>
-        <h2><span className="material-symbols-outlined">info</span>&nbsp;&nbsp;About</h2>
-      </LiquidGlass>
+      </div>
       <div className='content'>
         <div className='stats'>
 
