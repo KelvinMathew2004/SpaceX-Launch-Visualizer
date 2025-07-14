166282063b58f6b3936a878eb1cfef78ec154aa8
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-07-14 01:13:15 -0500

    Added conditional rendering for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 import logo from './assets/logo.png'
 import './components/LiquidGlass.css'
-import DistortionEffect from './components/DistortionEffect.jsx'
+import LiquidEffects from './components/LiquidEffects.jsx'
 import Launches from './components/Launches.jsx'
 
 function App() {
-  const [searchInput, setSearchInput] = useState("")
-  const [filter, setFilter] = useState("")
+  const [activeView, setActiveView] = useState('dashboard');
 
   return (
     <div className='whole-page'>
-      <DistortionEffect />
+      <LiquidEffects />
       <div className="wrapper">
         <div className="liquidGlass-wrapper sidenav">
           <div className="liquidGlass-effect"></div>
           <div className="liquidGlass-tint"></div>
-          <div className="liquidGlass-shine"></div>
+          <div className="liquidGlass-shine" id='sidenav-shine'></div>
           <div className="liquidGlass-text">
             <div className='logo'>
               <img src={logo} alt='SpaceX Logo' />
             </div>
-            <h2><span className="material-symbols-outlined">home</span>&nbsp;&nbsp;Dashboard</h2>
-            <h2><span className="material-symbols-outlined">search</span>&nbsp;&nbsp;Search</h2>
+            <h2 onClick={() => setActiveView('dashboard')}><span className="material-symbols-outlined">home</span>&nbsp;&nbsp;Dashboard</h2>
+            <h2 onClick={() => setActiveView('search')}><span className="material-symbols-outlined">search</span>&nbsp;&nbsp;Search</h2>
             <h2><span className="material-symbols-outlined">info</span>&nbsp;&nbsp;About</h2>
           </div>
         </div>
       </div>
 
-      <Launches />
+      <Launches 
+          activeView={activeView}
+      />
     </div>
   )
 }
