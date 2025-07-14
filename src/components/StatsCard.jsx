56f8912951b295096c039889574590e67f4ac3f4
Kelvin Mathew <kelvinmathew2004@gmail.com>
2025-07-14 01:15:31 -0500

    Rearranged classes and modified styling to fix issues

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -1,5 +1,5 @@
  const StatsCard = ({ title, value, subValue }) => (
-    <div className="stat-card">
+    <div>
         <p className="stat-title">{title}</p>
         <p className="stat-value">{value}</p>
         <p className="stat-sub-value">{subValue}</p>
